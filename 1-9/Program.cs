a200170c6fb99aec37393b3384c5218694d4f20f
jbg179 <jbg179@naver.com>
2022-09-26 22:10:49 +0900

    1-9

---
+++ 1-9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+class ThreadApp
+{
+    static void ThreadBody()
+    {
+        Console.WriteLine("In the thread body …");
+    }
+    public static void Main()
+    {
+        ThreadStart ts = new ThreadStart(ThreadBody);
+        Thread t = new Thread(ts);
+        Console.WriteLine("*** Start of Main");
+        t.Start();
+        Console.WriteLine("*** End of Main");
+    }
+}
