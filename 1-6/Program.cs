956bd3c43d2fa7e2951b7d896ac944daac52e41c
jbg179 <jbg179@naver.com>
2022-09-26 21:33:10 +0900

    1-6

---
+++ 1-6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+delegate void SampleDelegate();
+class DelegateClass
+{
+    public void DelegateMethod()
+    {
+        Console.WriteLine("In the DelegateClass.DelegateMethod ...");
+    }
+}
+class DelegateApp
+{
+    public static void Main()
+    {
+        DelegateClass obj = new DelegateClass();
+        SampleDelegate sd = new SampleDelegate(obj.DelegateMethod);
+        sd();
+    }
+}
