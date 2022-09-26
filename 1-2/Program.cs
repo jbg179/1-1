5139350ecaccbf349fb174919d752032aa9390e2
jbg179 <jbg179@naver.com>
2022-09-26 20:46:22 +0900

    1-2추가

---
+++ 1-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class PrimitiveTypeApp
+{
+    public static void Main()
+    {
+        bool b; int i = int.MaxValue;uint j=uint.MaxValue;
+        double d = double.MaxValue;
+        b = (i != 0);
+        Console.WriteLine("boolean b = " + b);
+        Console.WriteLine("Max value of signed integer = " + i);
+        Console.WriteLine("Max value of unsigned integer = " + j);
+        Console.WriteLine("Max value of double" + d);
+    }
+}
