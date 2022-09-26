c54e22d94b73109546e50d884cbdd6e98646f485
jbg179 <jbg179@naver.com>
2022-09-26 22:05:59 +0900

    1-8

---
+++ 1-7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class EventApp
+{
+    public event EventHandler MyEvent; // 2. 이벤트 선언
+    void MyEventHandler(object sender, EventArgs e)
+    { // 3. 이벤트 처리기 작성
+        Console.WriteLine("Hello world");
+    }
+    public EventApp()
+    { // 생성자
+        this.MyEvent += new EventHandler(MyEventHandler); // 4. 이벤트 처리기 등록
+    }
+    public void InvokeEvent()
+    {
+        if (MyEvent != null)
+            MyEvent(this, null); // 5. 이벤트 발생
+    }
+    public static void Main()
+    {
+        new EventApp().InvokeEvent();
+    }
+}
